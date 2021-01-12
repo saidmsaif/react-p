845eb0a4355965dd588349b167c4516a2a2965de
Saifullah Saif <asmsaif15@gmail.com>
2021-01-13 02:03:08 +0600

    Initial react app created

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css'
+import Profile from './profile/Profile'
+
+class App extends Component {
+    render() {
+        return <>
+            <div className="App-Container">
+                <h1>My First React Application</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ex temporibus totam quod cupiditate voluptatem laborum blanditiis, quia ducimus quae odit provident enim eligendi soluta labore eum adipisci earum odio.
+                </p>
+                    
+                <Profile />
+            </div>
+        </>
+    }
+}
+
+export default App 
