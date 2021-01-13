96fab1b12052ab2ffaafbc396eadd07dd9f54673
Saifullah Saif <asmsaif15@gmail.com>
2021-01-13 22:10:12 +0600

    All seperated profile component files has imported to this parent file

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Bio from './Bio'
+import Skills from './Skills'
+import SocialProfile from './SocialProfile'
+
 
 class Profile extends Component {
     render() {
         return <>
             <div className="Profile">
-                <Name />
+                <Bio />
                 <Skills />
                 <SocialProfile />
             </div>
