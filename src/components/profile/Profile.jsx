471c6f425440e5296a446edb6eda2d3e3dc0d855
Saifullah Saif <asmsaif15@gmail.com>
2021-01-13 23:57:25 +0600

    Render children from parent component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -3,6 +3,13 @@ import Bio from './Bio'
 import Skills from './Skills'
 import SocialProfile from './SocialProfile'
 
+const Child = props => (
+    <div>
+        <h1>This is Parent component</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, earum.</p>
+        {props.children}
+    </div>
+)
 
 class Profile extends Component {
     render() {
@@ -10,8 +17,12 @@ class Profile extends Component {
             <div className="Profile">
                 <Bio name='Saifullah' />
                 <Skills skillA='React' skillB='Express' skillC='MongoDB'/>
-                <SocialProfile linkA='Facebook' linkB='Twitter'/>
+                <SocialProfile linkA='Facebook' linkB='Twitter' />
             </div>
+
+            <Child>
+                <h2>I'm Children of the parent component</h2>
+            </Child>
         </>
     }
 }
