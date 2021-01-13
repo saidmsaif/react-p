56278cfdcd0820bf1fcad5595756fdda066fcbc9
Saifullah Saif <asmsaif15@gmail.com>
2021-01-13 22:02:20 +0600

    All functional components is now seperated with different file

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,33 +1,5 @@
 import React, { Component } from 'react'
 
-const Name = () => (
-    <div className="Bio">
-        <h1>Saifullah Saif</h1>
-    </div>
-)
-
-const Skills = () => (
-    <div className="Skills">
-        <h3>Skills :</h3>
-        <ol>
-            <li>WordPress Development</li>
-            <li>MERN Stack Development</li>
-            <li>E-Commerce Expert</li>
-        </ol>
-    </div>
-)
-
-const SocialProfile = () => (
-    <div className="Social-Links">
-        <h3>Social Links :</h3>
-        <ol>
-            <li><a href="https://www.facebook.com/saidmsaiff">Facebook</a></li>
-            <li><a href="https://www.twitter.com/saidmsaif">Twitter</a></li>
-            <li><a href="https://www.linkedin.com/saidmsaif">Facebook</a></li>
-        </ol>
-    </div>
-)
-
 class Profile extends Component {
     render() {
         return <>
