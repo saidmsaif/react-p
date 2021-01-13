6da80007c0065311c42c78efa5375e3cd3b2de88
Saifullah Saif <asmsaif15@gmail.com>
2021-01-13 22:06:14 +0600

    Skills will be placed here

---
+++ src/components/profile/Bio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Name = () => (
+    <div className="Bio">
+        <h1>Saifullah Saif</h1>
+    </div>
+)
+
+export default Bio
---
+++ src/components/profile/Skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Skills = () => (
+    <div className="Skills">
+        <h3>Skills :</h3>
+        <ol>
+            <li>WordPress Development</li>
+            <li>MERN Stack Development</li>
+            <li>E-Commerce Expert</li>
+        </ol>
+    </div>
+)
+
+export default Skills
