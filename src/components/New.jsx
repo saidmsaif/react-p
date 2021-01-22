b0965ee66172437592598ea54fb910883e1375da
S. Saif <saidmsaiff@gmail.com>
2021-01-23 00:57:46 +0600

    Simple Event Handler

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react'
 import classes from './stylesheet/style.module.css'
 
 export default class New extends Component {
+    handleButtonclick = (event) => {
+        console.log(event.target);
+    }
+    
     render() {
         return (
             <div>
-                <button className={classes.button}>Click Me</button>
+                <button
+                    className={classes.button}
+                    onClick={this.handleButtonclick}
+                >
+                    Click Me
+                </button>
             </div>
         )
     }
