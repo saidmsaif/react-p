fb7613d90dfc91aac4c601bdb8f09a808674b80b
S. Saif <saidmsaiff@gmail.com>
2021-01-26 03:17:03 +0600

    working with react form and it's getting error

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
 import { Col, Row } from 'react-bootstrap'
-import classes from './form.module.css'
+// import classes from './form.module.css'
 
 export default class Form extends Component {
+    state = {
+        name: '',
+        country: '',
+        birthday: '',
+        message: ''
+    };
+
+    handleChange = event => {
+        this.setState = ({
+            [event.target.name]: event.target.value
+        })
+    };
+
     render() {
+        const { name } = this.state;
+
         return (
             <div className=''>
                 <form action="">
@@ -11,14 +26,57 @@ export default class Form extends Component {
                         <Col>
                             <div className="form-group">
                                 <label htmlFor="">Name: </label>
-                                <input type="text" name="name" id="" placeholder='Enter your name' className='form-control'/>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id=""
+                                    value={name}
+                                    placeholder='Enter your name'
+                                    className='form-control'
+                                    onChange={this.handleChange}
+                                />
                             </div>
                         </Col>
 
                         <Col>
                             <div className="form-group">
-                                <label htmlFor="">Name: </label>
-                                <input type="text" name="name" id="" placeholder='Enter your name' className='form-control'/>
+                                <label htmlFor="country">Select your country : </label>
+                                <select name="country" id="" className='form-control' onChange={this.handleChange}>
+                                    <option>Select Your Country</option>
+                                    <option value="bd">Bangladesh</option>
+                                    <option value="it">Italy</option>
+                                    <option value="sw">Switzerland</option>
+                                    <option value="ind">India</option>
+                                    <option value="mly">Malaysia</option>
+                                </select>
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            <div className="form-group">
+                                <label htmlFor="birthday">Birthday : </label>
+                                <input type="date" name="birthday" id="" className='form-control' onChange={this.handleChange}/>
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            <label htmlFor="message">Your Message :</label>
+                            <textarea name="message" id="" className='form-control' onChange={this.handleChange}></textarea>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            <div className="text-center mt-3">
+                                <button type="button" value="Submit" className='btn btn-primary mr-2' onClick={() => {
+                                    console.log(this.state);
+                                }}>
+                                    click me </button>
+                                <input type="submit" value="Submit" className='btn btn-danger'/>
                             </div>
                         </Col>
                     </Row>
