7b56888d8fae4c4c0930203c1e5455ca96fd8d70
S. Saif <saidmsaiff@gmail.com>
2021-01-26 02:07:01 +0600

    New Form Markup

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
+import { Col, Row } from 'react-bootstrap'
 import classes from './form.module.css'
 
 export default class Form extends Component {
     render() {
         return (
-            <div className={classes.Container}>
-                <h1 className='display-4 text-center'>Working With React Form</h1>
-                <p className='lead text-center'>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, ea?
-                </p>
-                <hr />
+            <div className=''>
+                <form action="">
+                    <Row>
+                        <Col>
+                            <div className="form-group">
+                                <label htmlFor="">Name: </label>
+                                <input type="text" name="name" id="" placeholder='Enter your name' className='form-control'/>
+                            </div>
+                        </Col>
+
+                        <Col>
+                            <div className="form-group">
+                                <label htmlFor="">Name: </label>
+                                <input type="text" name="name" id="" placeholder='Enter your name' className='form-control'/>
+                            </div>
+                        </Col>
+                    </Row>
+                </form>
             </div>
         )
     }
