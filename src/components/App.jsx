10fb7466b471ff07c650ad76be8d0677c4cf2669
saidmsaif <saidmsaiff@gmail.com>
2021-03-24 17:04:35 +0600

    add input field and change some style of the page header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,22 +3,37 @@ import classes from "./App.module.css";
 import { Col, Container, Row } from 'react-bootstrap'
 
 class App extends Component {
+	state = {
+		searchTerm: ""
+	}
+
+	onChangeHandler = e => {
+		// TODO: Implement Later
+
+	}
+
+	onKeyPressHandler = e => {
+		// TODO: Implement Later
+		
+	}
+
     render() {
         return (
 			<>
 				<div
-					className={`${classes.Jumbo_BG} jumbotron jumbotron-fluid text-light text-center`}
-				>
-					<h1 className={`${classes.pageHeading} display-4 d-inline`}>
-						Working with React Form
-					</h1>
-					<p className='w-50 mx-auto mt-3'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Optio impedit, dolore earum numquam labore
-						exercitationem excepturi. Magnam commodi quos mollitia
-						reiciendis tempora? Doloribus, tempore nam. Ipsum magnam
-						cum id dolorum?
-					</p>
+					className={`${classes.Jumbo_BG} jumbotron jumbotron-fluid text-light text-left`}>
+					<Container>
+						<h1 className={`${classes.pageHeading} display-4 d-inline`}>
+							Simple Blog
+						</h1>
+						<p className='w-100 mt-3'>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Optio impedit, dolore earum numquam labore
+							exercitationem excepturi. Magnam commodi quos mollitia
+							reiciendis tempora? Doloribus, tempore nam. Ipsum magnam
+							cum id dolorum?
+						</p>
+					</Container>
 				</div>
 
 				{/* Main Container Area */}
@@ -26,7 +41,13 @@ class App extends Component {
 				<Container>
 					<Row>
 						<Col className='border p-5'>
-							
+							<input type="search"
+							className="form-control"
+								placeholder="Type anything and press enter..." 
+								value={this.state.searchTerm}
+								onChange={this.onChangeHandler}
+								onKeyPress={this.onKeyPressHandler}
+							/>
 						</Col>
 					</Row>
 				</Container>
