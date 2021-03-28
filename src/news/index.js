4e88c13e8534be937124be245aa64c156e5d4d9f
saidmsaif <saidmsaiff@gmail.com>
2021-03-29 02:55:30 +0600

    All previous files is deleted and new project setup is done

--- src/news/index.js
+++
@@ -1,9 +0,0 @@
-export const newsCategory = {
-  technology: 'technology',
-  science: 'science',
-  business: 'business',
-  entertainment: 'entertainment',
-  general: 'general',
-  health: 'health',
-  sports: 'sports',
-}
