f4d222ddcaf3335c15baeb6c5dabfc2645f75d89
saidmsaif <saidmsaiff@gmail.com>
2021-03-29 04:18:53 +0600

    Dynamically update category on the header

---
+++ src/News/index.js
@@ -0,0 +1,9 @@
+export const newsCategory = {
+  technology: ' technology',
+  business: ' business',
+  sports: ' sports',
+  science: ' science',
+  enertainment: 'ertainment',
+  general: ' general',
+  health: ' health',
+}
