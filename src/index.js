a7ab3d8226241503b67b7d07a29fcb97e091561a
saidmsaif <saidmsaiff@gmail.com>
2021-04-16 01:18:08 +0600

    Changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './components/App'
 
 ReactDOM.render(
