c3b5c3ab3285e0280cacbdbb66e6e072cc2703cb
saidmsaif <saidmsaiff@gmail.com>
2021-04-15 10:36:07 +0600

    Tailwind Congfig Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class App extends Component {
     render() {
         return <>
-            <h1>This is a text</h1>
+            <h1 className='bg-gray-300 text-9xl p-28'>This is a text</h1>
             </>
     }
 }
