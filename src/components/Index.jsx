80f7ea4ed369bbbb81065b07f17db95d9e110aa4
saidmsaif <saidmsaiff@gmail.com>
2021-04-24 00:08:11 +0600

    Delete old files and props practice

---
+++ src/components/Index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Index extends Component {
+    
+    render() {
+        return (
+			<>
+                <h1>{ this.props.name }</h1>
+			</>
+		);
+    }
+}
+
+export default Index
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css'
-import App from './components/App'
+import App from './App/App'
 
 ReactDOM.render(
   <App />,
