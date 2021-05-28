ec7b56adf2c7865852a8fe9b81085579972a1b77
saidmsaif <saidmsaiff@gmail.com>
2021-05-28 20:04:10 +0600

    Newly Initialized This App

---
+++ src/components/Index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Index extends Component {
+    render() {
+        return <>
+            <h1>this is a text</h1>
+            </>
+    }
+}
+
+export default Index
